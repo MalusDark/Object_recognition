bcb9f90ea61e1c000e764a03d03f4d7efb790005
MalusDark <mcdabestkot@yandex.ru>
2024-04-03 18:28:04 +0300

    Hamming

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace II_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(680, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Какая буква?";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(99, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(429, 244);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.comboBox1.Location = new System.Drawing.Point(553, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(635, 95);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Выбор изображения";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 297);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace II_4
+{
+    public partial class Form1 : Form
+    {
+        int x1, y1, x2, y2;
+        private Bitmap bmp;
+        private Pen blackPen;
+        static double[] p = new double[4];
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(50, 50);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            x2 = Convert.ToInt32(e.X); 
+            y2 = Convert.ToInt32(e.Y); 
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                blackPen = new Pen(Color.Black, comboBox1.SelectedIndex);
+                g.DrawLine(blackPen, x1, y1, x2, y2);
+                blackPen.Dispose();
+            }
+            pictureBox1.Image = bmp;
+            pictureBox1.Invalidate();
+            x1 = x2;
+            y1 = y2;
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x1 = Convert.ToInt32(e.X);
+            y1 = Convert.ToInt32(e.Y);
+            timer1.Start();
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                blackPen = new Pen(Color.Black, comboBox1.SelectedIndex);
+                g.DrawLine(blackPen, x1, y1, x2, y2);
+                blackPen.Dispose();
+            }
+            pictureBox1.Image = bmp;
+            pictureBox1.Invalidate();
+            x1 = x2;
+            y1 = y2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            string filename = openFileDialog1.FileName;
+            MessageBox.Show("Файл открыт");
+            pictureBox1.Image = Image.FromFile(filename);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            pictureBox1.Image = null;
+            bmp = new Bitmap(50, 50);
+            for(int i = 0; i<4; i++)
+            {
+                p[i] = 0;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int[] counter = new int[4];
+            for (int k = 1; k <= 5; k++)
+            {
+                Bitmap drawlink = new Bitmap(pictureBox1.Image);
+                Bitmap load = new Bitmap(Image.FromFile("C:\\Users\\artem\\source\\repos\\II_4\\training_data\\O\\O_" + Convert.ToString(k) + ".png"));
+
+                for (int i = 0; i < 50; i++)
+                    for (int j = 0; j < 50; j++)
+                    {
+                        if (drawlink.GetPixel(i, j) != load.GetPixel(i, j))
+                            counter[1]++;
+                    }
+                p[1] += 1000.0 / (1.0 + counter[1] * counter[1]);
+            }
+
+            listBox1.Items.Add($"Расстояние по Хэммингу: {counter[1]}");
+            listBox1.Items.Add($"Потенциал {p[1]}, что это буква O");
+            listBox1.Items.Add("");
+            for (int k = 1; k <= 5; k++)
+            {
+                Bitmap drawlink = new Bitmap(pictureBox1.Image);
+                Bitmap load = new Bitmap(Image.FromFile("C:\\Users\\artem\\source\\repos\\II_4\\training_data\\C\\C_" + Convert.ToString(k) + ".png"));
+
+                for (int i = 0; i < 50; i++)
+                {
+                    for (int j = 0; j < 50; j++)
+                    {
+                        if (drawlink.GetPixel(i, j) != load.GetPixel(i, j))
+                        {
+                            counter[0]++;
+                        }
+                    }
+                    p[0] += 1000.0 / (1.0 + counter[0] * counter[0]);
+                }
+            }
+
+            listBox1.Items.Add($"Расстояние по Хэммингу: {counter[0]}");
+            listBox1.Items.Add($"Потенциал {p[0]}, что это буква C");
+            listBox1.Items.Add("");
+
+            for (int k = 1; k <= 5; k++)
+            {
+                Bitmap drawlink = new Bitmap(pictureBox1.Image);
+                Bitmap load = new Bitmap(Image.FromFile("C:\\Users\\artem\\source\\repos\\II_4\\training_data\\P\\П_" + Convert.ToString(k) + ".png"));
+
+                for (int i = 0; i < 50; i++)
+                    for (int j = 0; j < 50; j++)
+                    {
+                        if (drawlink.GetPixel(i, j) != load.GetPixel(i, j))
+                            counter[2]++;
+                    }
+                p[2] += 1000.0 / (1.0 + counter[2] * counter[2]);
+            }
+
+            listBox1.Items.Add($"Расстояние по Хэммингу: {counter[2]}");
+            listBox1.Items.Add($"Потенциал {p[2]}, что это буква П");
+            listBox1.Items.Add("");
+
+            for (int k = 1; k <= 5; k++)
+            {
+                Bitmap drawlink = new Bitmap(pictureBox1.Image);
+                Bitmap load = new Bitmap(Image.FromFile("C:\\Users\\artem\\source\\repos\\II_4\\training_data\\R\\Р_" + Convert.ToString(k) + ".png"));
+
+                for (int i = 0; i < 50; i++)
+                    for (int j = 0; j < 50; j++)
+                    {
+                        if (drawlink.GetPixel(i, j) != load.GetPixel(i, j))
+                            counter[3]++;
+                    }
+                p[3] += 1000.0 / (1.0 + counter[3] * counter[3]);
+            }
+            listBox1.Items.Add($"Расстояние по Хэммингу: {counter[3]}");
+            listBox1.Items.Add($"Потенциал {p[3]}, что это буква Р");
+            listBox1.Items.Add("");
+            double maxP=0;
+            int maxX = 10000000;
+            for(int k = 0;k <= 3; k++)
+            {
+                if (maxP < p[k])
+                {
+                    maxP = p[k];
+                }
+                if(maxX > counter[k])
+                {
+                    maxX = counter[k];
+                }
+            }
+            string Symbol =" ";
+            string SymbolX = " ";
+            if(maxP == p[0])
+            {
+                Symbol = "C";
+            }
+            if (maxP == p[1])
+            {
+                Symbol = "O";
+            }
+            if (maxP == p[2])
+            {
+                Symbol = "П";
+            }
+            if (maxP == p[3])
+            {
+                Symbol = "Р";
+            }
+            if (maxX == counter[0])
+            {
+                SymbolX = "C";
+            }
+            if (maxX == counter[1])
+            {
+                SymbolX = "O";
+            }
+            if (maxX == counter[2])
+            {
+                SymbolX = "П";
+            }
+            if (maxX == counter[3])
+            {
+                SymbolX = "Р";
+            }
+            listBox1.Items.Add($"Результат по Потенциалу : буква "+Symbol);
+            listBox1.Items.Add($"Результат по Хэммингу : буква " + SymbolX);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+    }
+}
